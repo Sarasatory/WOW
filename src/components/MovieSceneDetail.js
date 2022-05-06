af66b67671b3c46fd52593bad868da69d6f8ac24
Satory <sarasatorynb@gmail.com>
2022-05-07 01:12:34 +0200

    Change component file MovieSceneDetail

--- src/components/MovieSceneDetail.js
+++ src/components/MovieSceneDetail.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 
 const MovieSceneDetail = (props) => {
-  return (
-    <>
+  let html = '';
+
+  if (!props.sceneFound) {
+    html = <h1>Está la cosa chunga, no hay nada aquí :o</h1>;
+  } else {
+    html = (
       <Link to={'/'}>
         <h1>Hola tusha ^^</h1>
         <h1>{props.sceneFound.id}</h1>
       </Link>
-    </>
-  );
+    );
+  }
+
+  return <>{html}</>;
 };
 export default MovieSceneDetail;
